33568c77a6fc6175dd6bab4da271c6904951d855
Danya <den.martsinovich@yandex.ru>
2019-02-17 16:25:29 +0300

    flask_img

--- flask_test.py
+++ flask_test.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -7,6 +7,13 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return "Привет, Яндекс! Я - Даня"
+
+
+@app.route('/image_sample')
+def image():
+    return '''<h1>Цветы</h1>
+    <img src="{}" alt="здесь должна была быть картинка, 
+    но не нашлась">'''.format(url_for('static', filename='img/test.jpeg'))
 
 
 if __name__ == '__main__':
