4b72c5f91389d1a677486462f2bdf49fe79f852e
Danya Martsinovich <den.martsinoivch@yandex.ru>
2019-02-27 17:20:26 +0300

    odd_even2

--- odd_even.py
+++ odd_even.py
@@ -7,10 +7,13 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route('/odd_even', methods=['POST', 'GET'])
 def sample_file_upload():
     print(request.method)
-    if request.method == 'POST':
+    if request.method == 'GET':
         return render_template('odd_even_form.html')
-    elif request.method == 'GET':
-        return render_template('odd_even.html', number=request.number)
+    elif request.method == 'POST':
+        try:
+            return render_template('odd_even.html', number=int(request.form['number']))
+        except ValueError:
+            return request.form['number'] + ' не является корректным целым числом'
 
 
 if __name__ == '__main__':
