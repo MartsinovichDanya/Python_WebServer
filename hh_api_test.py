a0e5f70acc11347b1e8b80abbbd4861fc9ee0e66
Danya <den.martsinovich@yandex.ru>
2019-03-14 00:44:40 +0300

    hh api

--- hh_api_test.py
+++ hh_api_test.py
@@ -3,10 +3,8 @@ x = []
 all_zp = 0
 all_n = 0
 for i in range(10):
-    # запрос
     url = 'https://api.hh.ru/vacancies'
     par = {'text': 'Python Junior', 'area': '113', 'per_page': '1', 'page': i}
     r = requests.get(url, params=par)
-    print(r)
     e = r.json()
-    print(e)
+    print(e['items'][0]['alternate_url'])
